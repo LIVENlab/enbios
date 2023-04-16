1e2a817dc69ad0439dccbdd12373ada5ec081d7d
ra <transfluxus@posteo.de>
2023-04-16 20:03:25 +0200

    added lcia_implementation_to_csv

--- test/classic_enbios/1_lci_to_nis.py
+++ test/classic_enbios/1_lci_to_nis.py
@@ -6,15 +6,15 @@ base_folder = Path("/home/ra/PycharmProjects/enbios2/data/enbios/_1_")
 base_in_folder = base_folder / "input"
 output_file = (base_folder / "output/output.xlsx").as_posix()
 
-s2n = SpoldToNIS()
-
 spold_files_folder = (base_in_folder / "SPOLDS").as_posix()
 nis_base_path = (base_in_folder / "BASELINE_UPDATE_APOS.xlsx").as_posix()
-
 correspondence_path = ""
-s2n.spold2nis("generic_energy_production",
-              spold_files_folder,
-              correspondence_path,
-              nis_base_path,
-              None,
-              output_file)
+
+if __name__ == "__main__":
+    s2n = SpoldToNIS()
+    s2n.spold2nis("generic_energy_production",
+                  spold_files_folder,
+                  correspondence_path,
+                  nis_base_path,
+                  None,
+                  output_file)
---
+++ test/classic_enbios/_2_lcia_implementation_to_csv.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import List
+
+from input.data_preparation.lcia_implementation_to_nis import convert_lcia_implementation_to_nis
+
+base_folder = Path("/home/ra/PycharmProjects/enbios2/data/enbios/_2_")
+lcia_implementation_file: str = (base_folder / "LCIA_Implementation_v3_8.xlsx").as_posix()
+lcia_file: str = (base_folder / "output/lcia_method_out.csv").as_posix()
+
+method_like: str = ""
+method_is: List[str] = None
+include_obsolete: bool = False
+use_nis_name_syntax: bool = True
+
+if isinstance(method_is, str):
+    method_is = [method_is]
+convert_lcia_implementation_to_nis(lcia_implementation_file, lcia_file,
+                                   method_like, method_is,
+                                   include_obsolete, use_nis_name_syntax)
