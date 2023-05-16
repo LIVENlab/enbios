9e53270e460be904983c3f87a64f0251501bc010
ra <Ramin.Soleymani@uab.cat>
2023-05-16 14:54:14 +0200

    add comments

--- enbios2/experiment/fast_tree.py
+++ enbios2/experiment/fast_tree.py
@@ -24,23 +24,32 @@ def get_tree(code: str, keep_exchange_type: list[str] = None, check_unique_code:
     if check_unique_code:
         check_unique_codes()
 
+    # all visited nodes (codes)
     visited = set()
-
+    # nodes to visit next
     to_visit = {code}
+    # nodes to visit in the next iteration
     to_visit_next = set()
+    # all exchanges (eventually filtered by type)
     all_exchanges = []
 
     while len(to_visit):
+        # get all exchanges that we could currently reach
         exchanges = list(ExchangeDataset.select().where(ExchangeDataset.output_code.in_(to_visit)))
+        # add nodes that we did not visit yet for the next iteration
         for exc in exchanges:
             if exc.input_code not in visited:
                 to_visit_next.add(exc.input_code)
+        # updated visited nodes
         visited.update(to_visit)
+        # save exchanges
         if not keep_exchange_type:
             all_exchanges.extend(exchanges)
         else:
             all_exchanges.extend([exc for exc in exchanges if exc.type in keep_exchange_type])
+        # update nodes to visit for next iteration
         to_visit = to_visit_next.copy()
+        # reset nodes to visit in next iteration
         to_visit_next.clear()
 
         print(len(visited), len(to_visit), len(all_exchanges))
@@ -50,4 +59,4 @@ def get_tree(code: str, keep_exchange_type: list[str] = None, check_unique_code:
 print(random_act)
 
 
-visited, exchanges = get_tree(random_act.key[1], ["technosphere"])
+visited, exchanges = get_tree(random_act["code"], ["technosphere"])
