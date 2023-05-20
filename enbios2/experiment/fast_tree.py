13e8b7f2bc804b0421efc6f82b8a4c5804de4038
ra <transfluxus@posteo.de>
2023-05-20 02:19:39 +0200

    hierarchy tree and first project around that...

--- enbios2/experiment/fast_tree.py
+++ enbios2/experiment/fast_tree.py
@@ -1,16 +1,15 @@
 import json
-from typing import Tuple, Optional
+from typing import Tuple
 
-from bw2data.backends import ActivityDataset, ExchangeDataset, Activity
-from bw2data import Database, databases, methods, config, projects
+from bw2data import Database, projects
+from bw2data.backends import ActivityDataset, ExchangeDataset
 
-from enbios2.bw2.util import info_exchanges, iter_activities_by_codes, iter_exchange_by_ids
-from enbios2.experiment.bw2sigma import grap_nodes
+from enbios2.bw2.util import iter_activities_by_codes
 
 # print(projects)
 projects.set_current("ecoi_dbs")
 db = Database("cutoff391")
-random_act = db.random()
+
 
 
 def check_unique_codes():
@@ -89,64 +88,62 @@ def get_tree_with_levels(code: str,
 
     # all visited nodes (codes)
     visited: set[str] = set[str]()
-    visited_with_level: set[tuple[str, int]] = set()
+    # keep a dict of code -> level
+    level: dict[str, int] = dict()
+    # keep a dict. code -> all input codes
+    inputs: dict[str, set[str]] = dict()
     # nodes to visit next
     to_visit = {code}
     # nodes to visit in the next iteration
-    to_visit_next = set()
     # all exchanges (eventually filtered by type). just ids for memory efficiency
     all_exchanges: list[int] = []
     current_level: int = 0
 
     while len(to_visit) and max_level != 0:
+        print("---")
         # get all exchanges that we could currently reach
+        visited.update(to_visit)
+        for v in to_visit:
+            level[v] = current_level
+            inputs[v] = set()
+
         exchanges = list(
             ExchangeDataset.select(ExchangeDataset.id,
                                    ExchangeDataset.input_code,
                                    ExchangeDataset.output_code,
                                    ExchangeDataset.type).where(
                 ExchangeDataset.output_code.in_(to_visit)))
+
+        to_visit.clear()
         # add nodes that we did not visit yet for the next iteration
         for exc in exchanges:
             # ignore if production
             if exc.input_code == exc.output_code:
                 continue
             if exc.input_code not in visited:
-                to_visit_next.add(exc.input_code)
+                to_visit.add(exc.input_code)
+                inputs[exc.output_code].add(exc.input_code)
             else:
                 # get level of visited node
-                c_l: Optional[tuple[str, int]] = None
-                for code, level in visited_with_level:
-                    if code == exc.output_code:
-                        c_l = (code, level)
-                if not c_l:
-                    print("ERROR: could not find level of visited node", code)
-                else:
-                    visited_with_level.remove(c_l)
-                    visited_with_level.add((c_l[0], current_level))
-                # change the level of the node
-        # updated visited nodes
-        visited.update(to_visit)
-        visited_with_level.update([(code, current_level) for code in to_visit])
+                def rec_inc(node: str, new_level):
+                    level[node] = new_level
+                    for inp in inputs.get(node, []):
+                        rec_inc(inp, new_level + 1)
+
+                rec_inc(exc.input_code, level[exc.output_code] + 1)
         # save exchanges
         if not keep_exchange_type:
             all_exchanges.extend([e.id for e in exchanges])
         else:
             all_exchanges.extend([exc.id for exc in exchanges if exc.type in keep_exchange_type])
-        # update nodes to visit for next iteration
-        to_visit = to_visit_next.copy()
-        # print(to_visit)
-        print([ActivityDataset.get(ActivityDataset.code == v).name for v in to_visit])
-        # reset nodes to visit in next iteration
-        to_visit_next.clear()
-
         print(f"visited: {len(visited)}, total exchanges: {len(all_exchanges)}, next nodes: {len(to_visit)}")
         max_level -= 1
         current_level += 1
     # sort by the second element of the tuple (level)
-    return sorted(visited_with_level, key=lambda x: x[1]), all_exchanges
+    return level, inputs, all_exchanges
 
-
+# random_act = db.random()
+random_act = db.get("46875148bb5fbac9cad7452d020a80de")
 print(random_act)
 # print(info_exchanges(random_act))
 
@@ -154,14 +151,14 @@ calc = True
 # 'non-ionic surfactant production, ethylene oxide derivate' (kilogram, GLO, None)
 
 if calc:
-    visited_with_levels, exchanges = get_tree_with_levels(random_act["code"], max_level=3)
-    json.dump((list(visited_with_levels), list(exchanges)), open("temp.json", "w"))
+    levels, inputs, exchanges = get_tree_with_levels(random_act["code"], max_level=3)
+    # json.dump((list(visited_with_levels), list(exchanges)), open("temp.json", "w"))
 else:
     visited_with_levels, exchanges = json.load(open("temp.json", "r"))
 
-visited, levels = zip(*visited_with_levels)
-activity_iter = iter_activities_by_codes(iter(visited))
+# visited, levels = zip(*visited_with_levels)
+# activity_iter = iter_activities_by_codes(iter(visited))
 
-graph = grap_nodes(activity_iter, iter_exchange_by_ids(iter(exchanges)), level_infos=levels)
-json.dumps(graph)
-json.dump(graph, open("/home/ra/PycharmProjects/enbios2/node_viz/sigma.js/examples/layouts/data.json", "w"))
+# graph = grap_nodes(activity_iter, iter_exchange_by_ids(iter(exchanges)), level_infos=levels)
+# json.dumps(graph)
+# json.dump(graph, open("/home/ra/PycharmProjects/enbios2/node_viz/sigma.js/examples/layouts/data.json", "w"))
