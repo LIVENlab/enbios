15a4744300a21140b3de5ce034e4c8c7ef517143
ra <transfluxus@posteo.de>
2023-05-31 10:54:05 +0200

    add db_fields module

---
+++ enbios2/base/db_fields.py
@@ -0,0 +1,74 @@
+import json
+import pickle
+
+from peewee import BlobField, SqliteDatabase, TextField
+
+
+class PickleField(BlobField):
+    def db_value(self, value):
+        return super(PickleField, self).db_value(pickle.dumps(value, protocol=4))
+
+    def python_value(self, value):
+        return pickle.loads(bytes(value))
+
+
+class SubstitutableDatabase:
+    def __init__(self, filepath, tables):
+        self._filepath = filepath
+        self._tables = tables
+        self._database = self._create_database()
+
+    def _create_database(self):
+        db = SqliteDatabase(self._filepath)
+        for model in self._tables:
+            model.bind(db, bind_refs=False, bind_backrefs=False)
+        db.connect()
+        db.create_tables(self._tables)
+        return db
+
+    @property
+    def db(self):
+        return self._database
+
+    def change_path(self, filepath):
+        self.db.close()
+        self._filepath = filepath
+        self._database = self._create_database()
+
+    def atomic(self):
+        return self.db.atomic()
+
+    def execute_sql(self, *args, **kwargs):
+        return self.db.execute_sql(*args, **kwargs)
+
+    def transaction(self):
+        return self.db.transaction()
+
+    def vacuum(self):
+        print("Vacuuming database ")
+        self.execute_sql("VACUUM;")
+
+
+class JSONField(TextField):
+    """Simpler JSON field that doesn't support advanced querying and is human-readable"""
+
+    def db_value(self, value):
+        return super().db_value(
+            json.dumps(
+                value,
+                ensure_ascii=False,
+                indent=2,
+                default=lambda x: x.isoformat() if hasattr(x, "isoformat") else x,
+            )
+        )
+
+    def python_value(self, value):
+        return json.loads(value)
+
+
+class TupleJSONField(JSONField):
+    def python_value(self, value):
+        data = json.loads(value)
+        if isinstance(data, list):
+            data = tuple(data)
+        return data
