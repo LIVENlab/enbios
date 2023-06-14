6c4f1f6a518c0f4da34ce2a22b302e0bdfffc179
ra <Ramin.Soleymani@uab.cat>
2023-06-14 15:49:22 +0200

    fixed unit conversion of activities for scenarios

--- test/enbios2/test_experiment_run.py
+++ test/enbios2/test_experiment_run.py
@@ -4,8 +4,10 @@ from enbios2.base.experiment import Experiment
 from enbios2.models.experiment_models import ExperimentData
 
 
-def test_simple():
-    data = {
+@pytest.fixture
+def scenario_run_basic1():
+    # Create and initialize the object
+    data = {"scenario": {
         "bw_project": "uab_bw_ei39",
         "activities_config": {
             "default_database": "ei391"
@@ -18,7 +20,7 @@ def test_simple():
                     "location": "DK"
                 },
                 "output": [
-                    "MWh",
+                    "kWh",
                     1
                 ]
             }
@@ -33,30 +35,51 @@ def test_simple():
                 "single_activity"
             ]
         }
-    }
-
-    expected_result_tree = {'name': 'root',
-                            'children': [{'name': 'energy',
-                                          'children': [
-                                              {'name': 'single_activity',
-                                               'children': [],
+    },
+        "expected_result_tree": {'name': 'root',
+                                 'children': [{'name': 'energy',
+                                               'children': [
+                                                   {'name': 'single_activity',
+                                                    'children': [],
+                                                    'data': {('EDIP 2003 no LT',
+                                                              'non-renewable resources no LT',
+                                                              'zinc no LT'): 6.169154556662401e-06}}],
                                                'data': {('EDIP 2003 no LT',
                                                          'non-renewable resources no LT',
                                                          'zinc no LT'): 6.169154556662401e-06}}],
-                                          'data': {('EDIP 2003 no LT',
-                                                    'non-renewable resources no LT',
-                                                    'zinc no LT'): 6.169154556662401e-06}}],
-                            'data': {('EDIP 2003 no LT',
-                                      'non-renewable resources no LT',
-                                      'zinc no LT'): 6.169154556662401e-06}}
+                                 'data': {('EDIP 2003 no LT',
+                                           'non-renewable resources no LT',
+                                           'zinc no LT'): 6.169154556662401e-06}}}
 
-    result = Experiment(ExperimentData(**data)).run()
-    assert result["default scenario"] == expected_result_tree
+    return data
 
+
+def test_simple(scenario_run_basic1):
+    scenario_data = scenario_run_basic1["scenario"]
+    result = Experiment(ExperimentData(**scenario_data)).run()
+    # print(result["default scenario"]["data"][('EDIP 2003 no LT', 'non-renewable resources no LT', 'zinc no LT')])
+    assert result["default scenario"] == scenario_run_basic1["expected_result_tree"]
+
+
+def test_scaled_demand(scenario_run_basic1):
+    scale = 3
+    scenario_data = scenario_run_basic1["scenario"]
+    scenario_data["activities"]["single_activity"]["output"] = ["kWh", scale]
+    expected_tree = scenario_run_basic1["expected_result_tree"]
     method = ('EDIP 2003 no LT', 'non-renewable resources no LT', 'zinc no LT')
-    # change the output
-    data["activities"]["single_activity"]["output"] = ["MWh", 3]
-    expected_value = result["default scenario"]["data"][method] * 3
-    # run again, and compare with some tolerance
-    result = Experiment(ExperimentData(**data)).run()
+    expected_value = expected_tree["data"][method] * scale
+    result = Experiment(ExperimentData(**scenario_data)).run()
+    # print(result["default scenario"]["data"][method])
     assert result["default scenario"]["data"][method] == pytest.approx(expected_value, abs=1e-10)
+
+
+def test_scaled_demand_unit(scenario_run_basic1):
+    scenario_data = scenario_run_basic1["scenario"]
+    scenario_data["activities"]["single_activity"]["output"] = ["MWh", 3]
+    expected_tree = scenario_run_basic1["expected_result_tree"]
+    method = ('EDIP 2003 no LT', 'non-renewable resources no LT', 'zinc no LT')
+    expected_value = expected_tree["data"][method] * 3000
+    result = Experiment(ExperimentData(**scenario_data)).run()
+    # print(result["default scenario"]["data"][method])
+    # print(result["default scenario"]["data"][method] / expected_value)
+    assert result["default scenario"]["data"][method] == pytest.approx(expected_value, abs=1e-5)
