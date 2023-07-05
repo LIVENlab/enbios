e7149aea3729a99b516945d950931504a68b4796
ra <Ramin.Soleymani@uab.cat>
2023-07-05 15:50:50 +0200

    adjust test

--- test/enbios2/test_experiment_run.py
+++ test/enbios2/test_experiment_run.py
@@ -1,6 +1,12 @@
-import pytest
+import pickle
+from pathlib import Path
 
-from enbios2.base.experiment import Experiment
+import pytest
+from deprecated.classic import deprecated
+
+from enbios2.base.experiment import Experiment, ScenarioResultNodeData
+from enbios2.const import BASE_DATA_PATH
+from enbios2.generic.tree.basic_tree import BasicTreeNode
 from enbios2.models.experiment_models import ExperimentData
 from test.enbios2.test_project_fixture import TEST_BW_PROJECT, TEST_BW_DATABASE
 
@@ -40,15 +46,23 @@ def scenario_run_basic1(default_bw_config):
                                                'children': [
                                                    {'name': 'single_activity',
                                                     'children': [],
-                                                    'data': {
-                                                        'EDIP 2003 no LT_non-renewable resources no LT_zinc no LT': 6.169154556662401e-06}}],
-                                               'data': {
-                                                   'EDIP 2003 no LT_non-renewable resources no LT_zinc no LT': 6.169154556662401e-06}}],
-                                 'data': {
-                                     'EDIP 2003 no LT_non-renewable resources no LT_zinc no LT': 6.169154556662401e-06}}
+                                                    'data':
+                                                        ScenarioResultNodeData(
+                                                            output=("kilowatt_hour", 1.0),
+                                                            results={
+                                                                'EDIP 2003 no LT_non-renewable resources no LT_zinc no LT': 6.169154556662401e-06})}],
+                                               'data': ScenarioResultNodeData(
+                                                   output=("kilowatt_hour", 1.0),
+                                                   results={
+                                                       'EDIP 2003 no LT_non-renewable resources no LT_zinc no LT': 6.169154556662401e-06})}],
+                                 'data': ScenarioResultNodeData(
+                                     output=("kilowatt_hour", 1.0),
+                                     results={
+                                         'EDIP 2003 no LT_non-renewable resources no LT_zinc no LT': 6.169154556662401e-06})}
     }
 
 
+@deprecated(reason="Use test_project_fixture")
 @pytest.fixture
 def default_bw_config() -> dict:
     return {
@@ -67,23 +81,55 @@ def default_method_tuple() -> tuple:
     return 'EDIP 2003 no LT', 'non-renewable resources no LT', 'zinc no LT'
 
 
+@pytest.fixture
+def temp_csv_file() -> Path:
+    path = BASE_DATA_PATH / "temp/test_csv.csv"
+    if path.exists():
+        path.unlink()
+    return path
+
+
 def test_single_lca_compare(scenario_run_basic1, default_method_tuple, default_method_str):
     experiment = Experiment(ExperimentData(**scenario_run_basic1["scenario"]))
     result = experiment.run()
     bw_activity = experiment.activitiesMap["single_activity"].bw_activity
-    expected_value = scenario_run_basic1["expected_result_tree"]["data"][default_method_str]
+    expected_value = scenario_run_basic1["expected_result_tree"]["data"].results[default_method_str]
     regular_score = bw_activity.lca(default_method_tuple).score
     assert regular_score == pytest.approx(expected_value, abs=1e-6)
-    print("cool")
+    assert regular_score == result[Experiment.DEFAULT_SCENARIO_ALIAS]["data"].results[default_method_str]
     # assert regular_score == result["default scenario"]["data"][default_method_str]
 
 
 def test_simple(scenario_run_basic1):
     scenario_data = scenario_run_basic1["scenario"]
     experiment = Experiment(ExperimentData(**scenario_data))
-    result = experiment.run()
+    results = experiment.run()[Experiment.DEFAULT_SCENARIO_ALIAS]
+    assert results == scenario_run_basic1["expected_result_tree"]
 
-    assert result["default scenario"] == scenario_run_basic1["expected_result_tree"]
+
+def test_pickle(scenario_run_basic1):
+    scenario_data = scenario_run_basic1["scenario"]
+    experiment = Experiment(ExperimentData(**scenario_data))
+    experiment.run()
+    pickle.dump(experiment, open(BASE_DATA_PATH / "temp/test_pickle.pickle", "wb"))
+
+
+def test_temp_load_pickle():
+    experiment = pickle.load(open(BASE_DATA_PATH / "temp/test_pickle.pickle", "rb"))
+    assert experiment
+
+    def recursive_resolve_outputs(node: BasicTreeNode[ScenarioResultNodeData]):
+        print("******")
+        print(node.name)
+
+    experiment.scenarios[0].result_tree.recursive_apply(recursive_resolve_outputs, depth_first=True, lazy=False)
+
+
+def test_csv_output(scenario_run_basic1, temp_csv_file):
+    scenario_data = scenario_run_basic1["scenario"]
+    experiment = Experiment(ExperimentData(**scenario_data))
+    experiment.run()
+    experiment.results_to_csv(temp_csv_file, Experiment.DEFAULT_SCENARIO_ALIAS)
 
 
 def test_scaled_demand(scenario_run_basic1, default_method_str: str):
@@ -91,21 +137,22 @@ def test_scaled_demand(scenario_run_basic1, default_method_str: str):
     scenario_data = scenario_run_basic1["scenario"]
     scenario_data["activities"]["single_activity"]["output"] = ["kWh", scale]
     expected_tree = scenario_run_basic1["expected_result_tree"]
-    expected_value = expected_tree["data"][default_method_str] * scale
+    expected_value = expected_tree["data"].results[default_method_str] * scale
     result = Experiment(ExperimentData(**scenario_data)).run()
     # print(result["default scenario"]["data"][method])
-    assert result["default scenario"]["data"][default_method_str] == pytest.approx(expected_value, abs=1e-10)
+    assert result[Experiment.DEFAULT_SCENARIO_ALIAS]["data"].results[default_method_str] == pytest.approx(
+        expected_value, abs=1e-10)
 
 
 def test_scaled_demand_unit(scenario_run_basic1, default_method_str: str):
     scenario_data = scenario_run_basic1["scenario"]
     scenario_data["activities"]["single_activity"]["output"] = ["MWh", 3]
     expected_tree = scenario_run_basic1["expected_result_tree"]
-    expected_value = expected_tree["data"][default_method_str] * 3000
+    expected_value = expected_tree["data"].results[default_method_str] * 3000
     result = Experiment(ExperimentData(**scenario_data)).run()
     # print(result["default scenario"]["data"][method])
     # print(result["default scenario"]["data"][method] / expected_value)
-    assert result["default scenario"]["data"][default_method_str] == pytest.approx(expected_value, abs=1e-5)
+    assert result[Experiment.DEFAULT_SCENARIO_ALIAS]["data"].results[default_method_str] == pytest.approx(expected_value, abs=1e-7)
 
 
 def test_scenario(scenario_run_basic1: dict, default_bw_config: dict, default_method_str: str):
@@ -153,7 +200,7 @@ def test_scenario(scenario_run_basic1: dict, default_bw_config: dict, default_me
 
     result = Experiment(ExperimentData(**scenario)).run()
     assert "scenario1" in result and "scenario2" in result
-    expected_value1 = scenario_run_basic1["expected_result_tree"]["data"][default_method_str]
-    assert result["scenario1"]["data"][default_method_str] == expected_value1
+    expected_value1 = scenario_run_basic1["expected_result_tree"]["data"].results[default_method_str]
+    assert result["scenario1"]["data"].results[default_method_str] == expected_value1
     expected_value2 = expected_value1 * 2000  # from 1KWh to 2MWh
-    assert result["scenario2"]["data"][default_method_str] == pytest.approx(expected_value2, abs=1e-9)
+    assert result["scenario2"]["data"].results[default_method_str] == pytest.approx(expected_value2, abs=1e-9)
