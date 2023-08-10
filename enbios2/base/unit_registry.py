b97d74c8efa0a69f3f94dba9458cd9616ae9dbd0
Ramin <Ramin.Soleymani@uab.cat>
2023-08-10 14:52:48 +0200

    fix ecoinvent_units_file_path creation

--- enbios2/base/unit_registry.py
+++ enbios2/base/unit_registry.py
@@ -8,5 +8,6 @@ ecoinvent_units_file_path = get_init_appdir() / "ecoinvent_pint_unit_match.txt"
 
 if not ecoinvent_units_file_path.exists():
     print(f"Creating 'ecoinvent_pint_unit_match' file at: {ecoinvent_units_file_path.as_posix()}")
+    ecoinvent_units_file_path.touch()
 
 ureg.load_definitions(ecoinvent_units_file_path)
