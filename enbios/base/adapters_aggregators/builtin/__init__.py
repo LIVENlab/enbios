931daefd7a1dd7cd3d0d0e508e48b915b03711b9
ra <transfluxus@posteo.de>
2024-02-21 15:24:44 +0100

    more mypy fixes

--- enbios/base/adapters_aggregators/builtin/__init__.py
+++ enbios/base/adapters_aggregators/builtin/__init__.py
@@ -1,10 +1,13 @@
+from typing import Type
+
+from enbios.base.adapters_aggregators.adapter import EnbiosAdapter
 from enbios.base.adapters_aggregators.builtin.sum_aggregator import SumAggregator
 from enbios.base.adapters_aggregators.builtin.simple_assignment_adapter import (
     SimpleAssignmentAdapter,
 )
 from enbios.bw2.brightway_experiment_adapter import BrightwayAdapter
 
-BUILTIN_ADAPTERS = {
+BUILTIN_ADAPTERS: dict[str, Type[EnbiosAdapter]] = {
     SimpleAssignmentAdapter.name(): SimpleAssignmentAdapter,
     BrightwayAdapter.name(): BrightwayAdapter,
 }
