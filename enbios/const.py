86057a850e67fd87158281732eca5205eef44791
ra <transfluxus@posteo.de>
2024-05-07 10:54:30 +0200

    fix PROJECT_PATH loop

--- enbios/const.py
+++ enbios/const.py
@@ -5,8 +5,10 @@ from pathlib import Path
 
 
 # Get the absolute path of the entry-point script
-PROJECT_PATH = Path(os.path.abspath(sys.argv[0]))
+PROJECT_PATH = Path(__file__)
 while PROJECT_PATH.name != "enbios":
+    if PROJECT_PATH == PROJECT_PATH.parent:
+        raise Exception("Could not find enbios project path")
     PROJECT_PATH = PROJECT_PATH.parent
 # cuz we have a enbios subfolder in the project...
 if PROJECT_PATH.parent.name == "enbios":
