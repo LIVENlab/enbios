f316028f76ea4701099c054fabc70813bb525718
ra <transfluxus@posteo.de>
2024-05-27 11:11:55 +0200

    update test_build.sh

--- scripts/test_build.sh
+++ scripts/test_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-. build.sh --no-update
-cd scripts || exit
+#. build.sh --no-update
+#cd scripts || exit
 . fresh_install.sh
+# go back to orig directory
+cd "$project_dir/scripts" || exit
 . fresh_install_py12.sh
